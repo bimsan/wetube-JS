e1de0d2a917894f917eb4d2bc78f9a12b9a30f29
sean <seankim21@naver.com>
2021-03-02 21:54:21 +0900

    wetube__2.8

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-import express from "express";
+import express, { Router } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http//localhost ${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
-
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -30,4 +20,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;

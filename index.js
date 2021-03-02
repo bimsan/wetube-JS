3737711c4ca203f7cc068bb99ce36f0b5c3d7dea
sean <seankim21@naver.com>
2021-03-02 18:42:21 +0900

    wetube__2.5

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http//localhost ${PORT}`);
-}
 
-function handleHome(req, res) {
-  res.send("Hi from home!!");
-}
+const handleHome = (req, res) => res.send("Hello from my ass");
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);

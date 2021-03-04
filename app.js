6efcd87faff9d9d1eed6db61449590c37ed643f9
sean <seankim21@naver.com>
2021-03-04 22:39:46 +0900

    wetube__2.16

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import { localsMiddleware } from "./middlewares";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
@@ -10,12 +11,13 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);

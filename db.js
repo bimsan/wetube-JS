480f816a9b1cc64e9c25a0fa60b970a3d6c24bb3
sean <seankim21@naver.com>
2021-03-08 21:45:18 +0900

    wetube__3.1

--- db.js
+++ db.js
@@ -1,54 +1,15 @@
-export const videos = [
-  {
-    id: 234431,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 30,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 133243,
-      name: "Sean",
-      email: "sean@kim.com",
-    },
-  },
-  {
-    id: 43553,
-    title: "Video super",
-    description: "This is something I love",
-    views: 30,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 133243,
-      name: "Sean",
-      email: "sean@kim.com",
-    },
-  },
-  {
-    id: 567468,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 30,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 133243,
-      name: "Sean",
-      email: "sean@kim.com",
-    },
-  },
-  {
-    id: 43926,
-    title: "Video cool",
-    description: "This is something I love",
-    views: 30,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 133243,
-      name: "Sean",
-      email: "sean@kim.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) =>
+  console.log(`❌  Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);

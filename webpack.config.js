49753544d6df9adf744bdbeee6e2018b375a0b9e
sean <seankim21@naver.com>
2021-03-17 23:01:35 +0900

    wetube__4.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
@@ -12,13 +13,20 @@ const config = {
     rules: [
       {
         test: /\.(scss)$/,
-        next: ExtractCSS.extract([
+        use: ExtractCSS.extract([
           {
             loader: "css-loader",
           },
           {
-              loader: "postcss-loader"
-          }
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins() {
+                  return [autoprefixer({ browsers: "cover 99.5%" })];
+                },
+              },
+            },
+          },
           {
             loader: "sass-loader",
           },
@@ -28,8 +36,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]",
+    filename: "[name].js",
   },
+  plugins: [new ExtractCSS("styles.css")],
 };
 
 module.exports = config;

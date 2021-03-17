d97a6e7580c23d6d99e674f8e01e6ea188b5972b
sean <seankim21@naver.com>
2021-03-18 00:10:44 +0900

    wetube__4.3__part.1

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
   mode: MODE,
   module: {
     rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
       {
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
@@ -41,4 +49,4 @@ const config = {
   plugins: [new ExtractCSS("styles.css")],
 };
 
-module.exports = config;
+module.exports = config({ devtool: "source-map" });

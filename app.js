a4c2cdb1fe9c4d981668bd676e0d89b86b416823
sean <seankim21@naver.com>
2021-03-19 21:02:15 +0900

    wetube__6.3

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import morgan from "morgan";
-import { localsMiddleware } from "./middlewares";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+
+import "./passport";
 
 const app = express();
 
@@ -27,6 +30,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- init.js
+++ init.js
@@ -1,16 +1,16 @@
-import "./db";
 import dotenv from "dotenv";
+import "./db";
 import app from "./app";
+
+dotenv.config();
 
 import "./models/Video";
 import "./models/Comment";
 import "./models/User";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`✅ 서버열링--: http://localhost:${PORT}`);
+  console.log(`✅ 범주니 서버열렸져염!!!: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
